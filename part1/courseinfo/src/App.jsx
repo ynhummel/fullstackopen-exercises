abe6895bd9e4a31f43b40718a2caa297198ebb92
Yuri Hummel <yurihummel@outlook.com>
2024-09-24 21:44:48 -0300

    ex1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,31 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.part[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.part[2]} {props.exercises[2]}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  const sum = props.exercises.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+
+  return <p>Number of exercises {sum}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +37,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
