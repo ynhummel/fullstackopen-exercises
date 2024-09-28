95c26cf2a7a9b04e8a9a9bbd76d67a1218b3bb8e
Yuri Hummel <yurihummel@outlook.com>
2024-09-28 14:34:23 -0300

    ex2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
