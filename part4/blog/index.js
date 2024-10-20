b9e4ce024c37d66e1ac0c077c64600a1084d68a1
Yuri Hummel <yurihummel@outlook.com>
2024-10-20 18:57:30 -0300

    ex4.1

---
+++ part4/blog/index.js
@@ -0,0 +1,43 @@
+import { } from 'dotenv/config'
+import express from 'express'
+const app = express()
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
