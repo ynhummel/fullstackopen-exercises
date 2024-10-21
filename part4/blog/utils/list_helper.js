5d9f172dc0411a2d043aa07d82da6ccb9f4c3a3c
Yuri Hummel <yurihummel@outlook.com>
2024-10-21 19:43:25 -0300

    ex4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,4 +4,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => acc + Number(curr.likes), 0)
 }
 
-export default { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  let mostLiked = { title: '', author: '', likes: 0 }
+
+  blogs.forEach((blog) => {
+    if (blog.likes >= mostLiked.likes) {
+      mostLiked = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes,
+      }
+    }
+  })
+
+  return mostLiked
+}
+
+export default { dummy, totalLikes, favoriteBlog }
