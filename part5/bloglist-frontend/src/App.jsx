2536b2571a651100a4515acfc1b903c71084bdf0
Yuri Hummel <yurihummel@outlook.com>
2024-10-31 15:04:38 -0300

    ex5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -16,16 +16,30 @@ const App = () => {
     })()
   }, [user])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('Wrong Credentials', username, password)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   const loginForm = () => {
@@ -46,14 +60,20 @@ const App = () => {
     )
   }
 
-  const blogList = () => {
-    return blogs.map(blog => <Blog key={blog.id} blog={blog} />)
+  const blogView = () => {
+    return (
+      <div>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+        {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+      </div>
+    )
   }
 
   return (
     < div >
       <h2>blogs</h2>
-      {user === null ? loginForm() : blogList()}
+      {user === null ? loginForm() : blogView()}
     </div >
   )
 }
